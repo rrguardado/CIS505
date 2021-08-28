f0dda9b019f8d71b7dd3e0f6643ac679656ceabf
Richard Guardado <rrguardado@my365.bellevue.edu>
2021-08-28 10:01:03 -0500

    Richard Guardado

--- cis-505/Module_1/HelloWorldApp/GuardadoHelloWorld.java
+++ cis-505/Module_1/HelloWorldApp/GuardadoHelloWorld.java
@@ -1,11 +1,23 @@
+/*
+References:
+
+Guardado, R. (2021). CIS 505 Intermediate Java Programming. Bellevue University.
+
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+*/
+
 package HelloWorldApp;
 
 public class GuardadoHelloWorld {
+
+    /**
+     * 
+     * Main method that prints "hello world" concatenated with two hard-coded string values to the terminal.
+     */
     public static void main(String[] args) {
+        String myFirstName = "Richard";        
         String myLastName = "Guardado";
-        String myFirstName = "Richard";
 
         System.out.printf("Hello World from " + myFirstName + " " + myLastName);
-    }
-    
-}
+    } // end main    
+} //end GuardadoHelloWorld
