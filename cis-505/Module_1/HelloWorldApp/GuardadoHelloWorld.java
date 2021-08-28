bd37f2161d58a2765dd10013e21c3dcbbba5b5e1
Richard Guardado <rrguardado@my365.bellevue.edu>
2021-08-28 09:35:16 -0500

    Richard Guardado's submission for assignment 1.2

---
+++ cis-505/Module_1/HelloWorldApp/GuardadoHelloWorld.java
@@ -0,0 +1,11 @@
+package HelloWorldApp;
+
+public class GuardadoHelloWorld {
+    public static void main(String[] args) {
+        String myLastName = "Guardado";
+        String myFirstName = "Richard";
+
+        System.out.printf("Hello World from " + myFirstName + " " + myLastName);
+    }
+    
+}
---
+++ cis-505/README.md
@@ -0,0 +1,2 @@
+# cis-505
+Richard Guardado's Bellevue University CIS 505 repo
