472a41994579154c2821369f52cece2863af0c67
rrguardado <89692007+rrguardado@users.noreply.github.com>
2021-09-08 18:25:30 -0500

    Add files via upload

---
+++ cis-505/Module_3/GuardadoHelloWorld copy.java
@@ -0,0 +1,23 @@
+/*
+References:
+
+Guardado, R. (2021). CIS 505 Intermediate Java Programming. Bellevue University.
+
+Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+*/
+
+package HelloWorldApp;
+
+public class GuardadoHelloWorld {
+
+    /**
+     * 
+     * Main method that prints "hello world" concatenated with two hard-coded string values to the terminal.
+     */
+    public static void main(String[] args) {
+        String myFirstName = "Richard";        
+        String myLastName = "Guardado";
+
+        System.out.printf("Hello World from " + myFirstName + " " + myLastName);
+    } // end main    
+} //end GuardadoHelloWorld
